791f7d8eda71a33eef541c3bb79d5dda0319e02c
chendl <chendelin@rantron.com>
2023-05-26 11:41:15 +0800

    sky

---
+++ skywalking-examples/src/main/java/com/delin/skywalking/SkyWalkingApplication.java
@@ -0,0 +1,19 @@
+package com.delin.skywalking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SkywalkingApplication
+ *
+ * @author: chendl
+ * @date: Created in 2023/5/26 11:17
+ * @description: com.delin.skywalking.SkywalkingApplication
+ */
+@SpringBootApplication
+public class SkyWalkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyWalkingApplication.class, args);
+    }
+}
